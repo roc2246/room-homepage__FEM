fcb877ade0c446cb6f527e8caaddae4930e07fbe
Riley <riley.childs@yahoo.com>
2023-05-11 19:51:33 -0400

    Finished creating toggles for slider images and placehoder arrays for slider headings and paragraphs

--- script.js
+++ script.js
@@ -18,3 +18,84 @@ nav.toggle.onclick = () => {
     nav.links.classList.remove("nav__links--mobile");
   }
 };
+
+const sliderImgMobile = document.getElementsByClassName("slider--mobile")[0];
+const sliderImgDesktop = document.getElementsByClassName("slider--desktop")[0];
+const sliderHeading = document.getElementsByClassName("top-blurb__heading")[0];
+const sliderParagraph = document.getElementsByClassName(
+  "top-blurb__paragraph"
+)[0];
+
+// mobile
+const sliderBackMobile =
+  document.getElementsByClassName("slider__container")[0];
+const sliderFowardMobile =
+  document.getElementsByClassName("slider__container")[1];
+
+// desktop
+const sliderBackDesktop =
+  document.getElementsByClassName("slider__container")[2];
+const sliderFowardDesktop =
+  document.getElementsByClassName("slider__container")[3];
+
+let imgNo = 1;
+
+const headings = [
+  "1",
+  "2",
+  "3"
+]
+
+const paragraphs = [
+  "1",
+  "2",
+  "3"
+]
+
+function setHeroImgs(screen, no) {
+  return `${screen}-image-hero-${no}.jpg`;
+}
+
+const mobileImage = function (no) {
+  return setHeroImgs("mobile", no);
+};
+
+const desktopImage = function (no) {
+  return setHeroImgs("desktop", no);
+};
+
+sliderFowardMobile.onclick = () => {
+  imgNo++;
+  if (imgNo > 3) imgNo = 1;
+  sliderImgMobile.style.backgroundImage = `url(images/${mobileImage(imgNo)})`;
+  sliderImgDesktop.style.backgroundImage = `url(images/${desktopImage(imgNo)})`;
+sliderHeading.innerHTML = headings[imgNo-1]
+sliderParagraph.innerHTML = paragraphs[imgNo-1]
+};
+
+sliderBackMobile.onclick = () => {
+  imgNo--;
+  if (imgNo < 1) imgNo = 3;
+  sliderImgMobile.style.backgroundImage = `url(images/${mobileImage(imgNo)})`;
+  sliderImgDesktop.style.backgroundImage = `url(images/${desktopImage(imgNo)})`;
+  sliderHeading.innerHTML = headings[imgNo-1]
+  sliderParagraph.innerHTML = paragraphs[imgNo-1]
+};
+
+sliderFowardDesktop.onclick = () => {
+  imgNo++;
+  if (imgNo > 3) imgNo = 1;
+  sliderImgMobile.style.backgroundImage = `url(images/${mobileImage(imgNo)})`;
+  sliderImgDesktop.style.backgroundImage = `url(images/${desktopImage(imgNo)})`;
+  sliderHeading.innerHTML = headings[imgNo-1]
+  sliderParagraph.innerHTML = paragraphs[imgNo-1]
+};
+
+sliderBackDesktop.onclick = () => {
+  imgNo--;
+  if (imgNo < 1) imgNo = 3;
+  sliderImgMobile.style.backgroundImage = `url(images/${mobileImage(imgNo)})`;
+  sliderImgDesktop.style.backgroundImage = `url(images/${desktopImage(imgNo)})`;
+  sliderHeading.innerHTML = headings[imgNo-1]
+  sliderParagraph.innerHTML = paragraphs[imgNo-1]
+};
