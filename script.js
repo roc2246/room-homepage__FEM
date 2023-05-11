ddb3dbbc24f05e45721615a00ff5b83058d63b47
Riley <riley.childs@yahoo.com>
2023-05-11 18:15:40 -0400

    Refactored mobile nav code

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const toggle = document.getElementsByClassName("nav__toggle")[0];
-const nav = document.getElementsByClassName("nav__links")[0];
-const hamburger = document.getElementsByClassName("nav__toggle--open")[0];
-const x = document.getElementsByClassName("nav__toggle--close")[0];
+const nav = {
+  toggle: document.getElementsByClassName("nav__toggle")[0],
+  links: document.getElementsByClassName("nav__links")[0],
+  icon: (img) => document.getElementsByClassName(`nav__toggle--${img}`)[0],
+  toggleNavIcon: function (link, open, close) {
+    this.links.style.display = link;
+    this.icon("open").style.display = open;
+    this.icon("close").style.display = close;
+  },
+};
 
-toggle.onclick = () => {
-  if (nav.style.display === "" || nav.style.display === "none") {
-    nav.style.display = "flex";
-    hamburger.style.display = "none";
-    x.style.display = "inline";
-    nav.classList.add("nav__links--mobile");
-  } else if (nav.style.display === "flex") {
-    nav.style.display = "none";
-    hamburger.style.display = "inline";
-    x.style.display = "none";
-    nav.classList.remove("nav__links--mobile");
+nav.toggle.onclick = () => {
+  if (nav.links.style.display === "" || nav.links.style.display === "none") {
+    nav.toggleNavIcon("flex", "none", "inline");
+    nav.links.classList.add("nav__links--mobile");
+  } else if (nav.links.style.display === "flex") {
+    nav.toggleNavIcon("none", "inline", "none");
+    nav.links.classList.remove("nav__links--mobile");
   }
 };
